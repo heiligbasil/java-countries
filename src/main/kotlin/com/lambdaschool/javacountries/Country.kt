146e95fd619c665eb96126074cfabdf954c9fc15
heiligbasil <basilief@yahoo.com>
2019-06-03 14:09:10 -0700

    Added the CountryList class

--- src/main/kotlin/com/lambdaschool/javacountries/Country.kt
+++ src/main/kotlin/com/lambdaschool/javacountries/Country.kt
@@ -1,3 +1,9 @@
 package com.lambdaschool.javacountries
 
-data class Country(val name: String, val population: Long, val size: Long, val age: Int)
+data class Country(val name: String, val population: Long, val size: Long, val age: Int) {
+
+    override fun toString(): String
+    {
+        return "Country(name='$name', population=$population, size=$size, age=$age)"
+    }
+}
